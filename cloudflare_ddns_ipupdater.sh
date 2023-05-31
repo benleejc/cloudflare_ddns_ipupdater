dd1ba324c2829f3fede2b621a9af83ac0c4dd8d6
benleejc <benleejc@gmail.com>
2023-06-01 01:06:10 +0800

    Add script to PUT new ip to cloudflare

--- cloudflare_ddns_ipupdater.sh
+++ cloudflare_ddns_ipupdater.sh
@@ -1,11 +1,13 @@
 #!bin/bash
+#source secrets file for variables
 source secrets.sh
 
 token=$token
 auth_email=$auth_email
-auth_type=$auth_type
+auth_type="global"
 zone_id=$zone_id
 record_name=$record_name
+proxied="false"
 
 # check existing record exists
 record=$(curl --request GET \
@@ -26,7 +28,9 @@ if [[ $record =~ \"count\":1 ]]; then
     echo "Record found!"
 fi
 
-
+# get existing variables from dns record
+old_ip=$(echo $record | sed -E 's/.*"content":"(([0-9]{1,3}\.){3}[0-9]{1,3})".*/\1/')
+record_id=$(echo "$record" | sed -E 's/.*"id":"([^,]+)".*/\1/')
 
 # get public ip
 ipv4_regex='([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])'
@@ -45,4 +49,25 @@ if [[ ! $ip =~ $ipv4_regex ]]; then
     exit 2
 fi
 
+# compare ip
+if [[ $ip == $old_ip ]]; then
+    logger -s "DDNS Updater: IP already exists. No change needed."
+    exit 0
+fi
+
+# update ip using API
+update=$(curl --request PUT \
+  --url "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$record_id" \
+  --header "Content-Type: application/json" \
+  --header "X-Auth-Email: $auth_email" \
+  --header "X-Auth-Key: $token" \
+  --data "{\"content\":\"$ip\",\"name\":\"$record_name\",\"proxied\":${proxied},\"type\":\"A\",\"ttl\": 3600}")
+
+# TODO send update to slack
+if [[ $update =~ \"success\":false\" ]]; then
+    logger -s "DDNS Updater: IP failed to update"
+    exit 1
+else
+    logger -s "DDNS Updater: $record_name IP updated from $old_ip to $ip"
+fi
 
